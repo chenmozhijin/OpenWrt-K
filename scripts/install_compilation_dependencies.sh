fccdd217f4aefe0f36530be3f6961043bdb9a702
沉默の金 <110812055+chenmozhijin@users.noreply.github.com>
2023-06-24 22:11:47 +0800

    Update install_compilation_dependencies.sh

--- scripts/install_compilation_dependencies.sh
+++ scripts/install_compilation_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo -E apt-mark hold grub-efi-amd64-signed
 sudo -E apt -y full-upgrade
-sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext \
+sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint b43-fwcutter binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext \
 gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev \
 libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils clang g++ python3-distutils \
-rsync unzip zlib1g-dev wget || sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar \
+rsync unzip zlib1g-dev wget || sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint b43-fwcutter binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar \
 flex gawk gettext gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev \
 libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils clang g++ \
 python3-distutils rsync unzip zlib1g-dev wget
