0debf4893417831e380ea04aea927f7718890340
沉默の金 <110812055+chenmozhijin@users.noreply.github.com>
2023-08-25 20:06:53 +0800

    工作流：防止清理空间出错导致的编译失败

--- scripts/clear_space.sh
+++ scripts/clear_space.sh
@@ -2,4 +2,5 @@
 sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc /etc/mysql /etc/php
 sudo -E apt-mark hold grub-efi-amd64-signed
 sudo -E apt update
-sudo -E apt -y purge azure-cli* docker* ghc* zulu* llvm* firefox google* dotnet* powershell* openjdk* mysql* php* mongodb* dotnet* moby* snap*
+sudo -E apt -y purge azure-cli* docker* ghc* zulu* llvm* firefox google* dotnet* powershell* openjdk* mysql* php* mongodb* dotnet* snap* moby* 2>&1
+exit 0
